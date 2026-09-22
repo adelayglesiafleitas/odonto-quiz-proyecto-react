70bc9aebf6525aaefe67f94d1dd3377a476142bc
Alejandro <adelayglesiafleitas@gmail.com>
2026-09-22 13:07:59 +0200

    80

--- src/App.tsx
+++ src/App.tsx
@@ -246,7 +246,7 @@ function App() {
     })
     navigate(RUTA.examen)
   }
-
+  
   if (userId && verifDispositivo === 'bloqueado') {
     return (
       <div className="mx-auto min-h-screen w-full max-w-md bg-background font-sans">
